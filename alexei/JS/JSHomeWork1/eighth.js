a0e4c1faead64dbd870adf017dff53fcbb1890ab
Astap_92 <alexei.atapencko@yandex.ru>
2022-11-17 18:26:27 +0300

    HomeWork JS 1

---
+++ alexei/JS/JSHomeWork1/eighth.js
@@ -0,0 +1,15 @@
+console.log('%cЗадание №8', 'padding: 20px 0; font-weight: bold; font-size: 18px')
+let a = 8
+let b = 3
+
+            // a+2(x-b)=16
+let x = b-a/2+8
+console.log('В уравнении a+2(x-b)=16 x=', x)
+
+            // b(x+15)=a+6x
+x = (a-15*b)/(b-6)
+console.log('В уравнении b(x+15)=a+6x x=', x)
+
+            // x+2x+ax+bx=23780
+x = 23780/(3+a+b)
+console.log('В уравнении x+2x+ax+bx=23780 x=', x)

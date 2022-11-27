73d573e60d26f0cd113ccf36f783a02f7258b61b
Alena Birulia <birulia.ip@gmail.com>
2022-11-27 23:00:36 +0300

    JS4

--- BiruliaAlena/JS/JS_4/script.js
+++ BiruliaAlena/JS/JS_4/script.js
@@ -22,19 +22,46 @@ for (let c = 23; c <= 57; c++) {
 console.log (arr3);
 console.log (sumArr3);
 
-//four 
+//four .includes(num.toString()[0]) ???
 let arr4 = ['10', '20', '30', '50', '235', '3000'];
-console.log (arr4.indexOf('1', '2', '5'));
-
-
-
+console.log(arr4.filter(num => ['1','2','5'].includes(num.toString()[0])))
 
 //five 
 
 let arr5 = [ 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 
+// six 
+let arr6 = ['HTML', 'CSS']
+arr6.push('JS');
+    console.log(arr6);
+    console.log(arr6[2]);
+
+/*seven
+const arr7 = [];
+let arrAnsver = +prompt('ваше любимое число?');
+
+for (let i = 0; i < arrAnsver; i++) {
+    arrAnsver = +prompt('ваше любимое число?');
+    arr7.push(arrAnsver);
+    }
+console.log(arr7);
+arr7.sort(function(a, b) {
+    return a - b
+});
+console.log(arr7);*/
+
+//eight
+let arr8 = [12, false, 'Текст', 4, 2, -5, 0];
+let arr8Reverse = arr8.reverse();
+    console.log (arr8Reverse);
 
 
+// nine 
+const arr9 = [5, 9, 21, , , 9, 78, , , , 6];
+
+//ten 
+
+//eleven
 
 
 

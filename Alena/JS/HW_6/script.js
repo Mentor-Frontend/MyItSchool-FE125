42f50cd7fd62079ba5bf9940d983ad17e45fd613
Ella-Mars <alenagreydunchikl@mail.ru>
2022-12-05 14:53:33 +0300

    Homework #6 JS

--- Alena/JS/HW_6/script.js
+++ Alena/JS/HW_6/script.js
@@ -1,35 +1,38 @@
 "use strict"
 //Task #1
-document.write("<span style='font-size:20px; font-weight:700'>Task #1</span>", '<br/>');
 let str1 = 'aaa@bbb@ccc';
-document.write(str1.replace(/@/g, '!'), '<br/>');
+console.log('Task #1: ', str1.replace(/@/g, '!'));
 //Task #2
-document.write("<span style='font-size:20px; font-weight:700'>Task #2</span>", '<br/>');
 let date = '2025-12-31';
-document.write(date.split('-').reverse().join('/'),'<br/>');
+console.log('Task #2: ', date.split('-').reverse().join('/'));
 //Task #3
-document.write("<span style='font-size:20px; font-weight:700'>Task #3</span>", '<br/>');
+console.log('Task #3: ');
 let str3 = 'Я учу javascript!';
-document.write(str3.substr(2,3), '<br/>');
-document.write(str3.substr(6,10), '<br/>');
-document.write(str3.substring(2,5), '<br/>');
-document.write(str3.substring(6,16), '<br/>');
-document.write(str3.slice(2,5), '<br/>');
-document.write(str3.slice(6,16), '<br/>');
+console.log(str3.substr(2, 3));
+console.log(str3.substr(6, 10));
+console.log(str3.substring(2, 5));
+console.log(str3.substring(6, 16));
+console.log(str3.slice(2, 5));
+console.log(str3.slice(6, 16));
 //Task #4
-document.write("<span style='font-size:20px; font-weight:700'>Task #4</span>", '<br/>');
 const arr4 = [4, 2, 5, 19, 13, 0, 10];
 let sum = 0;
-for(let i = 0; i < arr4.length; i++){
+for (let i = 0; i < arr4.length; i++) {
     sum += Math.pow(arr4[i], 3);
 }
-document.write(Math.pow(sum, 0.5), '<br/>');
+console.log('Task #4: ', Math.pow(sum, 0.5));
 //Task #5
-document.write("<span style='font-size:20px; font-weight:700'>Task #5</span>", '<br/>');
 let a = 3, b = 5;
-let c = Math.abs(a-b);
-document.write(c, '<br/>');
+let c = Math.abs(a - b);
+console.log('Task #5: ', c);
+//Task #6
+const date6 = new Date();
+function getZero(num) {
+    if (num < 10) return '0' + num;
+    else return num;
+}
+console.log('Task #6: ',date6.getHours() + ':' + date6.getMinutes() + ':' + date6.getSeconds() + ' '
+    + getZero(date6.getDate()) + '.' + getZero(date6.getMonth() + 1) + '.' + date6.getFullYear());
 //Task #7
-document.write("<span style='font-size:20px; font-weight:700'>Task #7</span>", '<br/>');
 let str7 = 'aa aba abba abbba abca abea';
-document.write(str7.match(/ab+a/g), '<br/>');
+console.log('Task #7: ', str7.match(/ab+a/g));

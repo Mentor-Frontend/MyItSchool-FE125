484d172df55e5d1da5c0b3a891c24e0a69875a09
Alena <117935297+Ella-Mars@users.noreply.github.com>
2023-02-22 00:07:14 +0300

    Delete .github directory

--- .github/JS/HW/hw_5/script5.js
+++
@@ -1,7 +0,0 @@
-function isEven (a) {
-    if (a % 2 == 0) {
-        console.log('true')
-    } 
-    else {console.log ('false')}
-}
-isEven(prompt('enter a'))
--- .github/JS/HW/hw_5/scrypt2.js
+++
@@ -1,6 +0,0 @@
-function getDerge(a) {
-    sqare = a**2 
-    return  a**3;
-       
-}
-console.log(getDerge(3), sqare)

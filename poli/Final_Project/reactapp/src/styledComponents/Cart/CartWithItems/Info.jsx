602ad3ee620490299da507f30c8b166681b5e9f4
pilla <PolliSims@yandex.ru>
2023-05-15 16:36:38 +0300

    final

--- poli/Final_Project/reactapp/src/styledComponents/Cart/CartWithItems/Info.jsx
+++ poli/Final_Project/reactapp/src/styledComponents/Cart/CartWithItems/Info.jsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 const Info = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    width: 316px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 316px;
 
   &.TotalPrice {
     border-top: 2px solid rgba(209, 209, 216, 1);
